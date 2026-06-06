244bc02f74e4c2f374e23cc7582cc11426303418
samrat-ghosh-007 <samrat9ghosh2018@gmail.com>
2026-06-06 15:29:55 +0530

    Fixed integration issues(3)

--- back/server.js
+++ back/server.js
@@ -52,7 +52,8 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use("/api/generate", generateRoute);
 app.use("/api/jobs", jobRoutes);
 app.use("/api/resume", resumeRoutes);
